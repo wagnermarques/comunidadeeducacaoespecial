6235b8cf8d9b1a3bf658224d6bb0faaee9dd4675
administrador <administrador@localhost.localdomain>
2021-11-25 16:43:53 -0300

    initial commit

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais nao conferem com nossos registros.',
+    'password' => 'A senha que você forneceu está incorreta.',
+    'throttle' => 'Número de tentativas de login excedeu o limite. Favor tentar de novo em :seconds segundos.',
+
+];
